782ca6b7f2491abc9dbfa7ed01c27e6852fab38f
steamhunter <steamhunter97@gmail.com>
2016-09-17 19:27:53 +0200

    steamlib integration

---
+++ steamfastgui/steam/libs/jsondownloader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Steam.libs
+{
+    public class jsondownloader
+    {
+        public class jsonrespond
+        {
+            public intrespond response { get; set; }
+        }
+        public class intrespond
+        {
+            public int total_count { get; set; }
+            public Games[] games { get; set; }
+
+        }
+        public class Games
+        {
+            public int appid { get; set; }
+            public string name { get; set; }
+            public int playtime_2weeks { get; set; }
+            public int playtime_forever { get; set; }
+            public string img_icon_url { get; set; }
+            public string img_logo_url { get; set; }
+        }
+        public jsonrespond getJSON()
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=8F83828068020B91BE744441CB0CC49B&steamid=76561198037767789&format=json&include_appinfo=1", "json.json");
+            }
+            string jsonstring = File.ReadAllText("json.json");
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            jsonrespond jres = ser.Deserialize<jsonrespond>(jsonstring);
+            return jres;
+        }
+    }
+}
---
+++ steamfastgui/steam/libs/steam.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Steam.libs;
+
+
+
+namespace Steam
+{
+    public class steam
+    {
+       private static string steamloc = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Valve\\Steam", "SteamPath", null).ToString();
+       private static string[] libary;
+       private static List<string> steamdirs = new List<string>();
+       public static List<string> gamenames = new List<string>();
+       private static List<string> gameids = new List<string>();
+        public struct SgameIconUrls
+        {
+            public int gameid;
+            public string iconurl;
+            public string name;
+        }
+        public static List<SgameIconUrls> Gameicourl = new List<SgameIconUrls>();
+        public static jsondownloader jdownl = new jsondownloader();
+       public static void loadsteam()
+        {
+            
+            libary = File.ReadAllLines(steamloc + "\\steamapps\\libraryfolders.vdf");
+            foreach (var item in libary)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    if (item.Contains("\""+i+"\""))
+                    {
+                        string sv = item.Trim().Remove(0, 3).Trim().Trim('"');
+                        steamdirs.Add(sv);
+
+                    }
+                }
+                
+            }
+            string[] games = { };
+            foreach (var item in steamdirs)
+            {
+                int lnght = item.Length + 11;
+                games = Directory.GetFiles(item + "\\steamapps");
+                for (int i = 0; i < games.Length; i++)
+                {
+                    string[] content = File.ReadLines(games[i]).ToArray();
+                    foreach (var cntnt in content)
+                    {
+                        if (cntnt.Contains("\"name\""))
+                        {
+                            string sv = cntnt.Trim().Remove(0, 6).Trim().Trim('"');
+
+                            gamenames.Add(sv);
+
+                        }
+                        if (cntnt.Contains("\"appid\""))
+                        {
+                            string sv = cntnt.Trim().Remove(0, 7).Trim().Trim('"');
+
+                            gameids.Add(sv);
+
+                        }
+                    }
+                }
+
+            }
+
+            for (int i = 1; i < gamenames.ToArray().Length; i++)
+            {
+                if (gamenames[i] == gamenames[i - 1])
+                {
+                    gamenames.Remove(gamenames[i]);
+                }
+            }
+            jsondownloader.jsonrespond respond = jdownl.getJSON();
+            foreach (var item in gameids)
+            {
+                for (int i = 0; i < respond.response.games.Count(); i++)
+                {
+                    if (respond.response.games[i].appid==int.Parse(item))
+                    {
+                        SgameIconUrls sgu = new SgameIconUrls();
+                        sgu.gameid = respond.response.games[i].appid;
+                        sgu.iconurl = respond.response.games[i].img_logo_url;
+                        sgu.name = respond.response.games[i].name;
+                        Gameicourl.Add(sgu);
+                    }
+                }
+            }
+            icodownloader.getnewicons(Gameicourl);
+        }
+       public static void startgame(int id)
+        {
+            
+            if (id == -1)
+            {
+               MessageBox.Show("nincs játék kiválasztva");
+            }
+            else
+            {
+                Process.Start(steam.steamloc + "\\steam.exe", " -applaunch " + steam.gameids[id]);
+            }
+        }
+        public static void startgamebyappid(int id)
+        {
+
+           
+                
+            
+                Process.Start(steam.steamloc + "\\steam.exe", " -applaunch " + id);
+            
+        }
+    }
+}
